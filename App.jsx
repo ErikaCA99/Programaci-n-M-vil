f888324d2b28a211afcf20d7a4ee4a1f246730e1
Erika Chino <erika.53chinoalanoca@gmail.com>
2024-11-26 20:01:15 -0400

    feat:cambios

--- App.jsx
+++ App.jsx
@@ -1,32 +1,8 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './src/Screens/Home';
-import LoginScreen from './src/Screens/LoginScreen';
-import Welcome from './src/Screens/Welcome';
+import React from 'react';
+import AppNavigation from './src/navigation/AppNavigation'; // Ruta correcta
 
-const Stack = createNativeStackNavigator();
+const App = () => {
+    return <AppNavigation />;
+};
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }} // Oculta el encabezado en la pantalla de inicio de sesión
-        />
-        <Stack.Screen
-          name="Welcome"
-          component={Welcome}
-          options={{ headerShown: false }} // Oculta el encabezado en la pantalla Welcome
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }} // Oculta el encabezado en la pantalla de inicio
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+export default App;
